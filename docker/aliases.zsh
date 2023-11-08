3b02333adb6c31dac92f0fd7270a4c19089e7151
Tim Park <timsjpark@gmail.com>
2023-11-08 15:16:27 -0700

    Add more docker aliases

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -6,3 +6,5 @@ alias dc-test="docker-compose run web test_all"
 alias dcbuild="op run --env-file=\"./.env\" docker-compose build"
 # Snapsheet docker dev
 alias dcbash="docker-compose exec app bash"
+alias workerup="dc exec app rake workflows:start_partitioned_worker -- --interval=5 --num-tasks=3"
+alias vbe="dc up -d app db"
